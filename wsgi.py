361e707f12ce80c1439279767082bc8b6cb5ef64
michaelgobz <raniahgobz@outlook.com>
2022-11-11 12:18:19 +0000

    production server

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+from web_flask.0-hello_route import app
+"""
+This file declire the wsgi application endpoint for the server
+
+"""
+
+if __name__ == "__main__":
+    app.run()
+
